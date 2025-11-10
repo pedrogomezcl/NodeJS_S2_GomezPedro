9ad979932619f98884a5a61a48fe86ba1faf5d85
pedrogomezcl <144635993+pedrogomezcl@users.noreply.github.com>
2025-11-10 13:14:08 +0000

    feat :sparkles: Encapsulamiento

---
+++ Dia3/Encapsulamiento/index.js
@@ -0,0 +1,14 @@
+// ###########
+// P.O.O - Encapsulamiento
+// ###########
+/*
+El encapsulamiento consiste en ocultar los detalles internos de una clase y exponer solo lo necesario 
+a través de una interfaz pública. En JavaScript esto se puede lograr mediante convenciones 
+(como usar _ para indicar que algo es privado), o más formalmente usando el símbolo # para declarar atributos verdaderamente privados.
+*/
+const Animal = require('./models/Animal');
+let animal1 = new Animal("Paco");
+console.log(animal1.nombre);
+animal1.verificarNombre("Paco");
+//❌
+animal1.scrambleNombre();
---
+++ Dia3/Encapsulamiento/models/Animal.js
@@ -0,0 +1,19 @@
+class Animal{
+    #nombre;
+    constructor(nombre){
+        this.#nombre=nombre;
+    }
+
+    verificarNombre(entrada){
+        if(entrada != this.#nombre){
+            console.log("No es el nombre del animal");
+        }
+        else{
+            console.log("Ese es el nombre del animal");
+        }
+    }
+    scrambleNombre(){
+        console.log(`aergupaj${this.#nombre}r&TGS)&F/(HWH)J)(#)`);
+    }
+}
+module.exports = Animal;
